38c6ee4576e54f22c07dd9eaf25e4888c95745bf
viskin <r.viskin@terikon.com>
2016-09-01 20:33:27 +0300

    started to implement iOS with Photos

--- src/ios/PhotoLibrary.swift
+++ src/ios/PhotoLibrary.swift
@@ -1,5 +1,62 @@
+import Photos
+
+class ResultImage {
+  var id: String?
+  var title: String?
+  var width: Int?
+  var height: Int?
+  var data: String?
+  var thumbnail: String?
+  var creationDate: Int?
+}
+
 @objc(PhotoLibrary) class PhotoLibrary : CDVPlugin {
-    
+
+  var fetchOptions: PHFetchOptions!
+
+  override func pluginInitialize() {
+    fetchOptions = PHFetchOptions()
+    fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+    if #available(iOS 9.0, *) {
+      fetchOptions.includeAssetSourceTypes = [.TypeUserLibrary, .TypeiTunesSynced, .TypeCloudShared]
+    }
+  }
+
+  // Will sort by creation date
+  func getPhotos(command: CDVInvokedUrlCommand) {
+    dispatch_async(dispatch_get_main_queue(), {
+
+      let fetchResult = PHAsset.fetchAssetsWithMediaType(.Image, options: self.fetchOptions)
+
+      var images = [ResultImage]()
+
+      fetchResult.enumerateObjectsUsingBlock {
+        (obj: AnyObject, idx: Int, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+        let asset = obj as! PHAsset
+        let resultImage = ResultImage()
+        resultImage.id = asset.localIdentifier
+        resultImage.title = ""
+        resultImage.width = asset.pixelWidth
+        resultImage.height = asset.pixelHeight
+        resultImage.data = ""
+        resultImage.thumbnail = ""
+        resultImage.creationDate = 0
+        images.append(resultImage)
+      }
+
+      let pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAsArray: images
+      )
+
+      self.commandDelegate!.sendPluginResult(
+        pluginResult,
+        callbackId: command.callbackId
+      )
+
+    });
+  }
+
   func echo(command: CDVInvokedUrlCommand) {
     var pluginResult = CDVPluginResult(
       status: CDVCommandStatus_ERROR
@@ -9,30 +66,30 @@
 
     if msg.characters.count > 0 {
       /* UIAlertController is iOS 8 or newer only. */
-      let toastController: UIAlertController = 
+      let toastController: UIAlertController =
         UIAlertController(
-          title: "", 
-          message: msg, 
+          title: "",
+          message: msg,
           preferredStyle: .Alert
         )
 
       self.viewController?.presentViewController(
-        toastController, 
-        animated: true, 
+        toastController,
+        animated: true,
         completion: nil
       )
 
       let duration = Double(NSEC_PER_SEC) * 3.0
-      
+
       dispatch_after(
         dispatch_time(
-          DISPATCH_TIME_NOW, 
+          DISPATCH_TIME_NOW,
           Int64(duration)
-        ), 
-        dispatch_get_main_queue(), 
-        { 
+        ),
+        dispatch_get_main_queue(),
+        {
           toastController.dismissViewControllerAnimated(
-            true, 
+            true,
             completion: nil
           )
         }
@@ -45,7 +102,7 @@
     }
 
     self.commandDelegate!.sendPluginResult(
-      pluginResult, 
+      pluginResult,
       callbackId: command.callbackId
     )
   }
--- www/PhotoLibrary.js
+++ www/PhotoLibrary.js
@@ -1,13 +1,24 @@
 var exec = require('cordova/exec');
 
-exports.echo = function(arg0, success, error) {
+exports.echo = function (arg0, success, error) {
   exec(success, error, "PhotoLibrary", "echo", [arg0]);
 };
 
-exports.echojs = function(arg0, success, error) {
-  if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+exports.echojs = function (arg0, success, error) {
+  if (arg0 && typeof (arg0) === 'string' && arg0.length > 0) {
     success(arg0);
   } else {
     error('Empty message!');
   }
+
 };
+
+exports.getPhotos = function (success, error) {
+  cordova.exec(
+    success,
+    error,
+    'PhotoLibrary',
+    'getPhotos',
+    []
+  );
+}
