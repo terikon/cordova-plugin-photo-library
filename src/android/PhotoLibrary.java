9b0a70076cd43062929be6273fe0cddea4bf8f55
viskin <r.viskin@terikon.com>
2016-10-03 15:24:11 +0300

    wrapped all the methods with getThreadPool

--- src/android/PhotoLibrary.java
+++ src/android/PhotoLibrary.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class PhotoLibrary extends CordovaPlugin {
 
   public static final String ACTION_GET_LIBRARY = "getLibrary";
@@ -30,29 +32,44 @@ public class PhotoLibrary extends CordovaPlugin {
       if (ACTION_GET_LIBRARY.equals(action)) {
         cordova.getThreadPool().execute(new Runnable() {
           public void run() {
-            // TODO
-            callbackContext.success();
+            try {
+              ArrayOfObjects library = getLibrary();
+              callbackContext.success(new JSONArray(library));
+            } catch (Exception e) {
+              e.printStackTrace();
+              callbackContext.error(e.getMessage());
+            }
           }
         });
         return true;
       } else if (ACTION_GET_THUMBNAIL.equals(action)) {
         cordova.getThreadPool().execute(new Runnable() {
           public void run() {
-            // TODO
-            callbackContext.success();
+            try {
+              // TODO
+              callbackContext.success();
+            } catch (Exception e) {
+              e.printStackTrace();
+              callbackContext.error(e.getMessage());
+            }
           }
         });
         return true;
       } else if (ACTION_GET_PHOTO.equals(action)) {
         cordova.getThreadPool().execute(new Runnable() {
           public void run() {
-            // TODO
-            callbackContext.success();
+            try {
+              // TODO
+              callbackContext.success();
+            } catch (Exception e) {
+              e.printStackTrace();
+              callbackContext.error(e.getMessage());
+            }
           }
         });
         return true;
       } else if (ACTION_STOP_CACHING.equals(action)) {
-        // TODO
+        // Nothing to do - it's ios only functionality
         callbackContext.success();
         return true;
       } else if (ACTION_ECHO.equals(action)) { // TODO: remove this
@@ -69,6 +86,22 @@ public class PhotoLibrary extends CordovaPlugin {
     }
   }
 
+  private ArrayOfObjects getLibrary() {
+    return null;
+  }
+
+  private void getThumbnail() {
+
+  }
+
+  private void getPhoto() {
+
+  }
+
+  private void stopCaching() {
+
+  }
+
   // TODO: remove this
   private void echo(String message, CallbackContext callbackContext) {
     if (message != null && message.length() > 0) {
@@ -77,5 +110,9 @@ public class PhotoLibrary extends CordovaPlugin {
       callbackContext.error("Expected one non-empty string argument.");
     }
   }
+
+  private class Object extends JSONObject { }
+
+  private class ArrayOfObjects extends ArrayList<Object> { }
 
 }
