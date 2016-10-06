1f8b5cfd5b541ba52e684a73c662286835a72f37
viskin <r.viskin@terikon.com>
2016-10-06 12:20:04 +0300

    android - now thumbnails are returned with exact size

--- src/android/PhotoLibrary.java
+++ src/android/PhotoLibrary.java
@@ -165,12 +165,16 @@ public class PhotoLibrary extends CordovaPlugin {
       InputStream is = getContext().getContentResolver().openInputStream(imageUri);
       BitmapFactory.decodeStream(is, null, options);
 
+      // get bitmap with size of closest power of 2
+      options.inSampleSize = calculateInSampleSize(options, thumbnailWidth, thumbnailHeight);
       options.inJustDecodeBounds = false;
-      options.inSampleSize = options.outHeight / thumbnailHeight;
       is = getContext().getContentResolver().openInputStream(imageUri);
-      bitmap = BitmapFactory.decodeStream(is, null, options);
-
+      Bitmap sampledBitmap = BitmapFactory.decodeStream(is, null, options);
       is.close();
+
+      // resize to exact size needed
+      bitmap = Bitmap.createScaledBitmap(sampledBitmap, thumbnailWidth, thumbnailHeight, true);
+      sampledBitmap.recycle();
     }
 
     // TODO: cache bytes
@@ -310,6 +314,30 @@ public class PhotoLibrary extends CordovaPlugin {
     }
 
     return byteBuffer.toByteArray();
+  }
+
+  // From https://developer.android.com/training/displaying-bitmaps/load-bitmap.html
+  public static int calculateInSampleSize(
+    BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+      final int halfHeight = height / 2;
+      final int halfWidth = width / 2;
+
+      // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+      // height and width larger than the requested height and width.
+      while ((halfHeight / inSampleSize) >= reqHeight
+        && (halfWidth / inSampleSize) >= reqWidth) {
+        inSampleSize *= 2;
+      }
+    }
+
+    return inSampleSize;
   }
 
   private class PictureData {
