2223c02721dd12f45ee0dc333be1886ecdf648b4
viskin <npm@terikon.com>
2016-10-16 17:26:14 +0300

    requestAuthorization implemented in ios

--- src/ios/PhotoLibrary.swift
+++ src/ios/PhotoLibrary.swift
@@ -5,11 +5,11 @@ import Foundation
     var service: PhotoLibraryService!
 
     override func pluginInitialize() {
-        
+
         service = PhotoLibraryService.instance
 
         NSURLProtocol.registerClass(PhotoLibraryProtocol)
-        
+
     }
 
 //    override func onMemoryWarning() {
@@ -19,13 +19,13 @@ import Foundation
     // Will sort by creation date
     func getLibrary(command: CDVInvokedUrlCommand) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            
+
             let options = command.arguments[0] as! NSDictionary
             let thumbnailWidth = options["thumbnailWidth"] as! Int
             let thumbnailHeight = options["thumbnailHeight"] as! Int
-            
+
             let library = self.service.getLibrary(thumbnailWidth, thumbnailHeight: thumbnailHeight)
-            
+
             let pluginResult = library != nil ?
                 CDVPluginResult(
                     status: CDVCommandStatus_OK,
@@ -34,9 +34,9 @@ import Foundation
                 CDVPluginResult(
                     status: CDVCommandStatus_ERROR,
                     messageAsString: self.service.PERMISSION_ERROR);
-            
+
             self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId)
-            
+
         }
     }
 
@@ -50,7 +50,7 @@ import Foundation
             let quality = options["quality"] as! Float
 
             self.service.getThumbnail(photoId, thumbnailWidth: thumbnailWidth, thumbnailHeight: thumbnailHeight, quality: quality) { (imageData) in
-                
+
                 let pluginResult = imageData != nil ?
                     CDVPluginResult(
                         status: CDVCommandStatus_OK,
@@ -59,11 +59,11 @@ import Foundation
                     CDVPluginResult(
                         status: CDVCommandStatus_ERROR,
                         messageAsString: self.service.PERMISSION_ERROR);
-                
+
                 self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId )
-                
+
             }
-            
+
         }
     }
 
@@ -73,7 +73,7 @@ import Foundation
             let photoId = command.arguments[0] as! String
 
             self.service.getPhoto(photoId) { (imageData) in
-                
+
                 let pluginResult = imageData != nil ?
                     CDVPluginResult(
                         status: CDVCommandStatus_OK,
@@ -82,17 +82,30 @@ import Foundation
                     CDVPluginResult(
                         status: CDVCommandStatus_ERROR,
                         messageAsString: self.service.PERMISSION_ERROR);
-                
+
                 self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId	)
 
             }
-            
+
         }
     }
 
     func stopCaching(command: CDVInvokedUrlCommand) {
 
         self.service.stopCaching()
+
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId	)
+
+    }
+
+    func requestAuthorization(command: CDVInvokedUrlCommand) {
+
+        let settingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
+        if let url = settingsUrl {
+            UIApplication.sharedApplication().openURL(url)
+            // TODO: run callback only when return ?
+        }
 
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
         self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId	)
