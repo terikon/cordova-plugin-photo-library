a6066a0db271bd06212942f272228554f07121f2
viskin <r.viskin@terikon.com>
2016-12-29 18:56:40 +0200

    added testImages visualization

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,22 @@
+var testImages = [
+  'Landscape_1.jpg',
+  'Landscape_2.jpg',
+  'Landscape_3.jpg',
+  'Landscape_4.jpg',
+  'Landscape_5.jpg',
+  'Landscape_6.jpg',
+  'Landscape_7.jpg',
+  'Landscape_8.jpg',
+  'Portrait_1.jpg',
+  'Portrait_2.jpg',
+  'Portrait_3.jpg',
+  'Portrait_4.jpg',
+  'Portrait_5.jpg',
+  'Portrait_6.jpg',
+  'Portrait_7.jpg',
+  'Portrait_8.jpg',
+];
+
 exports.defineAutoTests = function () {
 
   describe('cordova.plugins', function () {
@@ -87,7 +106,13 @@ exports.defineManualTests = function (contentEl, createActionButton) {
 
   var photo_library_tests = '<h3>Press requestAuthorization button to authorize storage</h3>' +
     '<div id="request_authorization"></div>' +
-    'Expected result: If authorized, this fact will be logged. On iOS: settings page will open. On Android: confirmation prompt will open.';
+    'Expected result: If authorized, this fact will be logged. On iOS: settings page will open. On Android: confirmation prompt will open.' +
+    '<h3>Press the button to visually inspect test-images</h3>' +
+    '<div id="inspect_test_images"></div>' +
+    'Expected result: All the images should be rotated right way'+
+    '<h3>Press the button to visually inspect thumbnails of test-images</h3>' +
+    '<div id="inspect_thumbnail_test_images"></div>' +
+    'Expected result: All the images should be rotated right way';
 
   contentEl.innerHTML = '<div id="info"></div>' + photo_library_tests;
 
@@ -102,5 +127,51 @@ exports.defineManualTests = function (contentEl, createActionButton) {
       }
     );
   }, 'request_authorization');
+
+  createActionButton('inspect test images', function () {
+    clearLog();
+    cordova.plugins.photoLibrary.getLibrary(
+      function (library) {
+        var found = 0;
+        library.forEach(function(libraryItem) {
+          if (testImages.includes(libraryItem.filename)) {
+            found += 1;
+            logMessage('<img src="' + libraryItem.photoURL + '" width="256">');
+          }
+        });
+        if (found < testImages.length) {
+          logMessage('Some test-images are missing. Please put photos from test-images folder to device library.')
+        }
+      },
+      function (err) {
+        logMessage('Error occured in getLibrary: ' + err);
+      }
+    );
+  }, 'inspect_test_images');
+
+  createActionButton('inspect thumbnail test images', function () {
+    clearLog();
+    cordova.plugins.photoLibrary.getLibrary(
+      function (library) {
+        var found = 0;
+        library.forEach(function(libraryItem) {
+          if (testImages.includes(libraryItem.filename)) {
+            found += 1;
+            logMessage('<img src="' + libraryItem.thumbnailURL + '" width="256">');
+          }
+        });
+        if (found < testImages.length) {
+          logMessage('Some test-images are missing. Please put photos from test-images folder to device library.')
+        }
+      },
+      function (err) {
+        logMessage('Error occured in getLibrary: ' + err);
+      },
+      {
+        thumbnailWidth: 256,
+        thumbnailHeight: 256,
+      }
+    );
+  }, 'inspect_thumbnail_test_images');
 
 };
