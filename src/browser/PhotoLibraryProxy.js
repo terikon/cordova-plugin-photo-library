7bc99fb610dd979734419cc2913c460bc83f01ab
Tien Nguyen <Tien Nguyen>
2020-06-19 14:39:46 +0700

    fix white screen issue in android

--- src/browser/PhotoLibraryProxy.js
+++ src/browser/PhotoLibraryProxy.js
@@ -1,6 +1,6 @@
 // Assume browser supports lambdas
 
-var async = cordova.require('cordova-plugin-photo-library.async');
+var async = cordova.require('cordova-plugin-photo-library-wkwebview.async');
 
 var photoLibraryProxy = {
 
